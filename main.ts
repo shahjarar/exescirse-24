f7ab1383e122735f2ae4a5b9ab0dbb9ac17f138d
shahjarar <160387866+shahjarar@users.noreply.github.com>
2024-02-29 18:02:57 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,62 @@
+// Task 24
+
+// more conditional tests: you don't have to limit the number of tests you  create to 10. if you want to try more comparisons,
+// write more tests, Have at least oned True and one False result for each of the following:
+
+// Tests for equality and inequality with strings
+
+let name_1 : string = "mehak"
+let name_2 : string = "mehak alamgir"
+let name_3 : string = "miss meh(ak alamgir"
+
+// if (name_1 == name_3){
+//     console.log("names are {equal")
+
+// } else {
+//      console.log("name are not equal")
+
+// }
+// if (name_1 != name_2){
+// //     console.log("names are equal")
+// }else{
+//     console.log("names are not equal")
+// // }
+// if (name_1 != name_2){
+//     console.log("name are equal")
+// // }
+
+// let age_1 : number = 18
+// let age_2 : number = 22
+
+// // if (age_1 == 18) {
+// //   console.log("aligible for vote")
+// // }
+
+// if (age_1 == 18) {
+//     console.log("aligible for vote in older category")
+//   }
+  
+// if (age_1 <= age_2){
+//     console.log("younger")
+// }
+
+// if (age_2 >= age_1){
+//     console.log("older")
+// }
+
+// if (age_1 == 18 && age_2 == 22){
+//     console.log("person is eligibale for vote")
+// }
+
+
+// if (age_1 == 18 || age_2 == 22){
+//     console.log("person is  not eligibale for vote")
+// // }
+
+let country : string [] = ["pakistan","india","japan","china"]
+
+//     console.log("pakitan is in country list")
+
+if(("america")){
+ console.log("america is not include in an array")
+}
